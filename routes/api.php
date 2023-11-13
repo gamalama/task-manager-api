2051bedcafa387a6680bea71394875a9a7e6e307
Fakhry <fakhry.linux@gmail.com>
2023-11-13 10:18:11 +0800

    Creating and Updating Projects API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -26,4 +27,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //Route::get('tasks', [TaskController::class, 'index']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('tasks', TaskController::class);
+    Route::apiResource('projects', ProjectController::class);
 });
