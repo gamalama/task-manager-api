2088d4a3192f403adf41de9604ede45fb9e530fd
Fakhry <fakhry.linux@gmail.com>
2023-11-13 15:47:20 +0800

    Show the Project Members

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -32,7 +32,7 @@ class ProjectController extends Controller
 
     public function show(Request $request, Project $project)
     {
-        return (new ProjectResource($project))->load('tasks');
+        return (new ProjectResource($project))->load('tasks')->load('members');
     }
 
     public function update(UpdateProjectRequest $request, Project $project)
