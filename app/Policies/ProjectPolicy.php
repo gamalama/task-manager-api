28136124d2dcace2ec0dae5583a4c741fb397121
Fakhry <fakhry.linux@gmail.com>
2023-11-13 16:25:26 +0800

    Filter List Responses by Membership

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -13,7 +13,7 @@ class ProjectPolicy
      */
     public function viewAny(User $user): bool
     {
-        //
+        return true;
     }
 
     /**
@@ -21,15 +21,7 @@ class ProjectPolicy
      */
     public function view(User $user, Project $project): bool
     {
-        if ($user->id === $project->creator_id) {
-            return true;
-        }
-
-        if ($user->memberships->contains($project)) {
-            return true;
-        }
-
-        return false;
+        return $user->memberships->contains($project);
     }
 
     /**
